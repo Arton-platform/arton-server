bfe87e21b5825d1ecc08c2c200d4849121432128
godcoder <dwrhksfl77@naver.com>
2023-01-11 01:31:37 +0900

    ART-14 찜 아티스트 추천 기능 구

---
+++ src/main/java/com/arton/backend/artist/adapter/out/repository/CustomArtistRepository.java
@@ -0,0 +1,11 @@
+package com.arton.backend.artist.adapter.out.repository;
+
+import com.arton.backend.artist.domain.Artist;
+import com.arton.backend.performance.domain.PerformanceType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CustomArtistRepository{
+    List<Artist> getArtistByPerformanceType(PerformanceType performanceType);
+}
--- src/main/java/com/arton/backend/artist/application/port/out/ArtistRepositoryPort.java
+++ src/main/java/com/arton/backend/artist/application/port/out/ArtistRepositoryPort.java
@@ -1,4 +1,9 @@
 package com.arton.backend.artist.application.port.out;
 
+import com.arton.backend.artist.domain.Artist;
+
+import java.util.List;
+
 public interface ArtistRepositoryPort {
+    List<Artist> getAllArtist();
 }
