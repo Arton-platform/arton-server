0fd68ad0d67c37cdaabb4cbe360de92de69ac2ae
송근봉 <thdrmsqhd@gmail.com>
2023-01-13 17:06:30 +0900

    ART-35 공지사항 전체 조회 완료

--- src/main/java/com/arton/backend/announcement/adapter/in/web/AnnounceController.java
+++ src/main/java/com/arton/backend/announcement/adapter/in/web/AnnounceController.java
@@ -2,7 +2,11 @@ package com.arton.backend.announcement.adapter.in.web;
 
 import com.arton.backend.announcement.application.port.in.SelectAllUseCase;
 import com.arton.backend.announcement.domain.Announcement;
+import com.arton.backend.infra.shared.common.ResponseData;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +16,19 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/announcement")
+@Slf4j
 public class AnnounceController {
 
     private final SelectAllUseCase selectAllUseCase;
 
     @GetMapping("/list")
-    public List<Announcement> announcementList(){
-        return selectAllUseCase.announcementList();
+    public ResponseEntity<ResponseData<List<Announcement>>> announcementList(){
+        log.info("[Announcement] : {}", "announcementList");
+        ResponseData<List<Announcement>> response = new ResponseData(
+          "SUCCESS",
+                HttpStatus.OK.value(),
+                selectAllUseCase.announcementList()
+        );
+        return ResponseEntity.ok().body(response);
     }
 }
