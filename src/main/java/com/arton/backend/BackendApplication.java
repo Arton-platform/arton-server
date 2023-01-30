f0836306e95c51b5ca6122ea7e4d4dfc32b04ce0
a60156077 <dwrhksfl77@naver.com>
2023-01-30 10:51:07 +0900

    fix-간편로그인 액세스 토큰 전달받아서 가입으로 변경/ GetMapping 실수 PostMapping으로 변경.

--- src/main/java/com/arton/backend/BackendApplication.java
+++ src/main/java/com/arton/backend/BackendApplication.java
@@ -10,6 +10,7 @@ import com.arton.backend.user.adapter.out.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -30,6 +31,7 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Profile("local")
 	@Bean
 	public TestDataInit testDataInit(ArtistRepository artistRepository, PerformanceRepository performanceRepository, UserRepository userRepository, FollowRepository followRepository,
 									 ReviewRepository reviewRepository, UserImageRepository userImageRepository, PasswordEncoder passwordEncoder) {
