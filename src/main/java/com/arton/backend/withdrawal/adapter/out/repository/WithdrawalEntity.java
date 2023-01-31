24672ffe15484b14b0b1661390473781a1d35313
a60156077 <dwrhksfl77@naver.com>
2023-01-31 16:52:48 +0900

    ART-29 회원탈퇴사유 mapper 작성.

--- src/main/java/com/arton/backend/withdrawal/adapter/out/repository/WithdrawalEntity.java
+++ src/main/java/com/arton/backend/withdrawal/adapter/out/repository/WithdrawalEntity.java
@@ -3,10 +3,12 @@ package com.arton.backend.withdrawal.adapter.out.repository;
 import com.arton.backend.infra.shared.BaseEntity;
 import com.arton.backend.user.adapter.out.repository.UserEntity;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 /**
  * 탈퇴사유 테이블
@@ -23,4 +25,12 @@ public class WithdrawalEntity extends BaseEntity {
     @JoinColumn(name = "user_id")
     private UserEntity user;
     private String comment;
+
+    @Builder
+    public WithdrawalEntity(LocalDateTime createdDate, LocalDateTime updateDate, Long id, UserEntity user, String comment) {
+        super(createdDate, updateDate);
+        this.id = id;
+        this.user = user;
+        this.comment = comment;
+    }
 }
--- src/main/java/com/arton/backend/withdrawal/domain/Withdrawal.java
+++ src/main/java/com/arton/backend/withdrawal/domain/Withdrawal.java
@@ -1,10 +1,7 @@
 package com.arton.backend.withdrawal.domain;
 
 import com.arton.backend.user.domain.User;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -23,4 +20,13 @@ public class Withdrawal {
     private LocalDateTime createdDate;
     /** 변경일 */
     private LocalDateTime updateDate;
+
+    @Builder
+    public Withdrawal(Long id, User user, String comment, LocalDateTime createdDate, LocalDateTime updateDate) {
+        this.id = id;
+        this.user = user;
+        this.comment = comment;
+        this.createdDate = createdDate;
+        this.updateDate = updateDate;
+    }
 }
