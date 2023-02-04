3e758d5356a379fdd52ff06c6564b3a2bbd8045a
godcoder <dwrhksfl77@naver.com>
2023-02-04 20:24:30 +0900

    ART-3 ElasticSearch Artist Document Builder, mapper 추가.

--- src/main/java/com/arton/backend/artist/adapter/out/persistence/entity/ArtistDocument.java
+++ src/main/java/com/arton/backend/artist/adapter/out/persistence/entity/ArtistDocument.java
@@ -2,6 +2,7 @@ package com.arton.backend.artist.adapter.out.persistence.entity;
 
 import com.arton.backend.performer.adapter.out.persistence.entity.PerformerEntity;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.elasticsearch.annotations.*;
@@ -9,7 +10,9 @@ import org.springframework.data.elasticsearch.annotations.*;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Document(indexName = "artist")
 @Mapping(mappingPath = "elastic/artist-mapping.json")
@@ -35,4 +38,29 @@ public class ArtistDocument {
     /** 수정일 */
     @Field(type = FieldType.Date, format = {DateFormat.date_hour_minute_second_millis, DateFormat.epoch_millis})
     private LocalDateTime updateDate;
+
+    @Builder
+    public ArtistDocument(Long id, String name, Integer age, String snsId, String profileImageUrl, List<PerformerEntity> performances, LocalDateTime createdDate, LocalDateTime updateDate) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.snsId = snsId;
+        this.profileImageUrl = profileImageUrl;
+        this.performances = performances;
+        this.createdDate = createdDate;
+        this.updateDate = updateDate;
+    }
+
+    public static ArtistDocument from(ArtistEntity artist) {
+        return ArtistDocument.builder()
+                .createdDate(artist.getCreatedDate())
+                .updateDate(artist.getUpdateDate())
+                .age(artist.getAge())
+                .name(artist.getName())
+                .profileImageUrl(artist.getProfileImageUrl())
+                .performances(Optional.ofNullable(artist.getPerformances()).orElseGet(Collections::emptyList))
+                .snsId(artist.getSnsId())
+                .id(artist.getId())
+                .build();
+    }
 }
