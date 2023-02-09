d664d24173b2c1e3ca5ae6f516f6f5eb2eeaec92
thdrmsqhd <thdrmsqhd@gmail.com>
2023-02-09 22:05:44 +0900

    ART-59 회원-관리-회원목록 테스트 코드 완료

--- src/main/java/com/arton/backend/follow/adapter/out/persistence/entity/FollowFeedEntity.java
+++ src/main/java/com/arton/backend/follow/adapter/out/persistence/entity/FollowFeedEntity.java
@@ -14,6 +14,7 @@ import javax.persistence.*;
 @Getter
 @Builder
 public class FollowFeedEntity extends BaseEntity {
+    @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
