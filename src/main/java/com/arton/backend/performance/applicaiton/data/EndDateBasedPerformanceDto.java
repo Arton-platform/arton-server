26c926f65da930c7ff969dcb3c6af979f8c07876
a60156077 <dwrhksfl77@naver.com>
2023-02-15 09:11:52 +0900

    ART-19 메인 페이지 DTO 변환 만들어야 함. - EndDateBasedPerformanceDto domainToDto 구현 완료.

--- src/main/java/com/arton/backend/performance/applicaiton/data/EndDateBasedPerformanceDto.java
+++ src/main/java/com/arton/backend/performance/applicaiton/data/EndDateBasedPerformanceDto.java
@@ -1,9 +1,13 @@
 package com.arton.backend.performance.applicaiton.data;
 
+import com.arton.backend.performance.domain.PerformanceType;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
 
-
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Schema(description = "곧 종료 공연을 위한 Dto")
+@ToString
 public class EndDateBasedPerformanceDto extends CommonPerformanceDto{
     @Schema(description = "장소")
     private String place;
@@ -11,4 +15,12 @@ public class EndDateBasedPerformanceDto extends CommonPerformanceDto{
     private String musicalDateTime;
     @Schema(description = "공연 종료일")
     private String endDate;
+
+    @Builder
+    public EndDateBasedPerformanceDto(Long id, String title, String imageUrl, PerformanceType performanceType, String place, String musicalDateTime, String endDate) {
+        super(id, title, imageUrl, performanceType);
+        this.place = place;
+        this.musicalDateTime = musicalDateTime;
+        this.endDate = endDate;
+    }
 }
