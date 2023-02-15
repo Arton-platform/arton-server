0f5ddb58313c098bb3f73572e48fd72ad0d8d639
a60156077 <dwrhksfl77@naver.com>
2023-02-15 14:15:14 +0900

    ART-19 메인 페이지 - mockMvc 테스트 완료.

--- src/test/java/com/arton/backend/user/application/service/MainPageServiceTest.java
+++ src/test/java/com/arton/backend/user/application/service/MainPageServiceTest.java
@@ -1,23 +1,60 @@
 package com.arton.backend.user.application.service;
 
 import com.arton.backend.artist.application.data.CommonArtistDto;
+import com.arton.backend.auth.application.data.LoginRequestDto;
 import com.arton.backend.user.application.data.MainPageDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
+@AutoConfigureMockMvc
 class MainPageServiceTest {
 
     @Autowired
     MainPageService mainPageService;
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Test
     void getMainPageTest() {
         MainPageDto mainPage = mainPageService.getMainPage(1L);
         System.out.println("mainPage = " + mainPage);
-        List<CommonArtistDto> artists = mainPage.getArtists();
+    }
+
+    @Test
+    void mvcTest() throws Exception {
+        LoginRequestDto loginRequestDto = LoginRequestDto.builder()
+                .email("j6731000@gmail.com")
+                .password("temp")
+                .build();
+
+        ResultActions perform = mockMvc.perform(MockMvcRequestBuilders.post("/auth/login")
+                .content(objectMapper.writeValueAsString(loginRequestDto))
+                .contentType(MediaType.APPLICATION_JSON));
+        MvcResult mvcResult = perform.andReturn();
+        String contentAsString = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
+        System.out.println("contentAsString = " + contentAsString);
+        String accessToken = objectMapper.readTree(contentAsString).get("accessToken").asText();
+
+        MvcResult home = mockMvc.perform(MockMvcRequestBuilders.get("/home").header("Authorization", "Bearer " + accessToken)).andReturn();
+        String contentAsString1 = home.getResponse().getContentAsString(StandardCharsets.UTF_8);
+        System.out.println("contentAsString1 = " + contentAsString1);
     }
 }
