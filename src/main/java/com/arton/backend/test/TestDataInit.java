550313f2c773937481161315ee06d1ce699b3863
godcoder <dwrhksfl77@naver.com>
2023-02-15 21:35:10 +0900

    공연상세보기 테스트 완료.

--- src/main/java/com/arton/backend/test/TestDataInit.java
+++ src/main/java/com/arton/backend/test/TestDataInit.java
@@ -89,6 +89,8 @@ public class TestDataInit {
         }
         String[] titles = {"이석훈", "이석훈 2021 연말 콘서트", "이석훈&SG워너비 동행", "김민수 연말콘서트"};
         String[] places = {"전주", "전라북도 전주", "서울", "서울 특별시"};
+        String[] musicalDates = {"2022.02.13~2022.02.15", "2023.02.01~2023.02.11"};
+        int[] ages = {8, 15, 19};
         LocalDateTime[] times = {LocalDateTime.now(), LocalDateTime.now().minusDays(5L), LocalDateTime.now().minusYears(1L), LocalDateTime.now().plusDays(2L)};
         for (int i = 0; i < 1000; i++) {
             ArtistEntity artist = ArtistEntity.builder()
@@ -111,6 +113,9 @@ public class TestDataInit {
                     .ticketOpenDate(times[random.nextInt(4)].minusDays(3L))
                     .ticketEndDate(times[random.nextInt(4)].minusDays(3L))
                     .hit((long)random.nextInt(1000)+1)
+                    .musicalDateTime(musicalDates[random.nextInt(2)])
+                    .purchaseLimit(3)
+                    .limitAge(ages[random.nextInt(3)])
                     .build();
             performances.add(performance);
 
