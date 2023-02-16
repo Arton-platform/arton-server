93f9c64b44a31f5a18dc1fb52c512711699a6f54
a60156077 <dwrhksfl77@naver.com>
2023-02-16 09:46:43 +0900

    ART-77-관리자 페이지 - 공연 생성을 위한 PerformanceCreateDto 생성

---
+++ src/main/java/com/arton/backend/performance/applicaiton/data/PerformanceCreateDto.java
@@ -0,0 +1,51 @@
+package com.arton.backend.performance.applicaiton.data;
+
+import com.arton.backend.performance.domain.PerformanceType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(description = "공연 생성을 위한 Dto")
+@ToString
+public class PerformanceCreateDto {
+    @Schema(description = "ID")
+    private Long performanceId;
+    @Schema(description = "제목")
+    private String title;
+    @Schema(description = "설명")
+    private String description;
+    @Schema(description = "공연 시작일")
+    private LocalDateTime startDate;
+    @Schema(description = "공연 종료일")
+    private LocalDateTime endDate;
+    @Schema(description = "상영시간")
+    private Integer runningTime;
+    @Schema(description = "휴식시간")
+    private Integer interMission;
+    @Schema(description = "제한시간")
+    private Integer limitTime;
+    @Schema(description = "제한나이")
+    private Integer limitAge;
+    @Schema(description = "예매 링크")
+    private String link;
+    @Schema(description = " 뮤지컬 or 콘서트")
+    private PerformanceType performanceType;
+    @Schema(description = "장소")
+    private String place;
+    @Schema(description = "공연일시 yyyy.MM.dd~yyyy.MM.dd")
+    private String musicalDateTime;
+    @Schema(description = "티켓팅 오픈일")
+    private LocalDateTime ticketOpenDate;
+    @Schema(description = "티켓팅 종료일")
+    private String ticketEndDate;
+    @Schema(description = "매수 제한")
+    private Integer purchaseLimit;
+}
