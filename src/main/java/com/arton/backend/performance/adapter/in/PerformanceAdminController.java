7c4419cde1b36af81966386cbc090f9712745282
a60156077 <dwrhksfl77@naver.com>
2023-02-16 11:01:33 +0900

    ART-77-관리자 페이지 - 공연 생성을 위한 메소드 구현.

--- src/main/java/com/arton/backend/performance/adapter/in/PerformanceAdminController.java
+++ src/main/java/com/arton/backend/performance/adapter/in/PerformanceAdminController.java
@@ -1,11 +1,16 @@
 package com.arton.backend.performance.adapter.in;
 
+import com.arton.backend.auth.application.data.SignupRequestDto;
 import com.arton.backend.performance.applicaiton.data.PerformanceCreateDto;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.awt.*;
 
 /**
  * 관리자만 접근 가능.
@@ -19,5 +24,10 @@ public class PerformanceAdminController {
     public String addPerformance(Model model) {
         model.addAttribute("performance", new PerformanceCreateDto());
         return "performance/createForm";
+    }
+
+    @PostMapping(value = "/add", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String postPerformance(@RequestPart(required = true, name = "performance") @Valid PerformanceCreateDto performanceCreateDto, @RequestPart(required = false, name = "images") MultipartFile[] multipartFiles) {
+        return "performance/index";
     }
 }
