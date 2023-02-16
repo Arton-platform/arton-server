6a9e0566f2b5de9c69ecc71a8faddf7dad590fe7
a60156077 <dwrhksfl77@naver.com>
2023-02-16 15:57:53 +0900

    ART-77-관리자 페이지 - thymleaf input time 받아서 처리하는 부분 구현.

--- src/main/java/com/arton/backend/performance/adapter/in/PerformanceAdminController.java
+++ src/main/java/com/arton/backend/performance/adapter/in/PerformanceAdminController.java
@@ -7,11 +7,10 @@ import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.time.temporal.TemporalField;
 
 /**
  * 관리자만 접근 가능.
@@ -28,9 +27,9 @@ public class PerformanceAdminController {
         return "performance/createForm";
     }
 
-    @PostMapping(value = "/add", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public String postPerformance(@RequestPart(required = true, name = "performance")PerformanceCreateDto performanceCreateDto, @RequestPart(required = false, name = "images") MultipartFile[] multipartFiles) {
-        System.out.println("performanceCreateDto = " + performanceCreateDto);
+    @PostMapping(value = "/add")
+    public String postPerformance(@ModelAttribute(name = "performance") PerformanceCreateDto performance, @RequestParam(required = false, name = "images") MultipartFile[] multipartFiles) {
+
         return "performance/index";
     }
 }
--- src/main/java/com/arton/backend/performance/applicaiton/data/PerformanceCreateDto.java
+++ src/main/java/com/arton/backend/performance/applicaiton/data/PerformanceCreateDto.java
@@ -1,18 +1,21 @@
 package com.arton.backend.performance.applicaiton.data;
 
+import com.arton.backend.performance.domain.Performance;
 import com.arton.backend.performance.domain.PerformanceType;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.*;
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Data
 @Schema(description = "공연 생성을 위한 Dto")
 @ToString
-public class PerformanceCreateDto {
+public class PerformanceCreateDto implements Serializable {
     @Schema(description = "ID")
     private Long performanceId;
     @Schema(description = "제목")
@@ -20,9 +23,11 @@ public class PerformanceCreateDto {
     @Schema(description = "설명")
     private String description;
     @Schema(description = "공연 시작일")
-    private LocalDateTime startDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
     @Schema(description = "공연 종료일")
-    private LocalDateTime endDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
     @Schema(description = "상영시간")
     private Integer runningTime;
     @Schema(description = "휴식시간")
@@ -37,12 +42,34 @@ public class PerformanceCreateDto {
     private PerformanceType performanceType;
     @Schema(description = "장소")
     private String place;
-    @Schema(description = "공연일시 yyyy.MM.dd~yyyy.MM.dd")
-    private String musicalDateTime;
     @Schema(description = "티켓팅 오픈일")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime ticketOpenDate;
     @Schema(description = "티켓팅 종료일")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime ticketEndDate;
     @Schema(description = "매수 제한")
     private Integer purchaseLimit;
+
+    public Performance dtoToDomain() {
+        return Performance.builder()
+                .purchaseLimit(getPurchaseLimit())
+                .performanceType(getPerformanceType())
+                .ticketOpenDate(getTicketOpenDate())
+                .ticketEndDate(getTicketEndDate())
+                .description(getDescription())
+                .title(getTitle())
+                .startDate(getStartDate().atStartOfDay())
+                .endDate(getEndDate().atTime(LocalTime.MAX))
+                .hit(0L)
+                .interMission(getInterMission())
+                .limitAge(getLimitAge())
+                .link(getLink())
+                .place(getPlace())
+                .runningTime(getRunningTime())
+                .starScore(0)
+                .limitTime(getLimitTime())
+                .musicalDateTime(getStartDate().toString()+"~"+getEndDate().toString())
+                .build();
+    }
 }
