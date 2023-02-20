a9f74c801282ca3037cfb093514a3144ea10d6d1
a60156077 <dwrhksfl77@naver.com>
2023-02-20 13:14:30 +0900

    ART-62 - 관리자 페이지에 사용할 검색 DTO 생성

---
+++ src/main/java/com/arton/backend/performance/applicaiton/data/PerformanceAdminSearchDto.java
@@ -0,0 +1,23 @@
+package com.arton.backend.performance.applicaiton.data;
+
+import com.arton.backend.performance.domain.PerformanceType;
+import com.arton.backend.performance.domain.ShowCategory;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(description = "관리자 페이지 검색 조건 Dto")
+@ToString
+public class PerformanceAdminSearchDto {
+    private ShowCategory showCategory;
+    private PerformanceType performanceType;
+    private String keyword;
+
+    @Builder
+    public PerformanceAdminSearchDto(ShowCategory showCategory, PerformanceType performanceType, String keyword) {
+        this.showCategory = showCategory;
+        this.performanceType = performanceType;
+        this.keyword = keyword;
+    }
+}
