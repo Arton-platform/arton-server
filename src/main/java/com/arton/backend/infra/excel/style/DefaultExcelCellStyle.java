19096ab2d847c1394214fceccccd1d4719cfe426
a60156077 <dwrhksfl77@naver.com>
2023-02-22 15:26:36 +0900

    ART-62 - 엑셀 저장 기능 구현중. - 추후 회원관리나 다른곳에서도 엑셀 저장이 필요할 것 같음. - 템플릿으로 작성하게 구현중 - 기본 셀스타일 지정, 커스텀 셀스타일 지정 클래스 생성.

--- src/main/java/com/arton/backend/infra/excel/style/DefaultExcelCellStyle.java
+++ src/main/java/com/arton/backend/infra/excel/style/DefaultExcelCellStyle.java
@@ -1,11 +1,36 @@
 package com.arton.backend.infra.excel.style;
 
+import com.arton.backend.infra.excel.style.align.DefaultExcelAlign;
+import com.arton.backend.infra.excel.style.align.ExcelAlign;
+import com.arton.backend.infra.excel.style.border.DefaultExcelBorders;
+import com.arton.backend.infra.excel.style.border.ExcelBorderStyle;
+import com.arton.backend.infra.excel.style.color.DefaultExcelColor;
+import com.arton.backend.infra.excel.style.color.ExcelColor;
 import org.apache.poi.ss.usermodel.CellStyle;
 
 public enum DefaultExcelCellStyle implements ExcelCellStyle {
 
+    GREY_HEADER(DefaultExcelColor.rgb(217, 217, 217),
+            DefaultExcelBorders.newInstance(ExcelBorderStyle.THIN),
+            DefaultExcelAlign.CENTER_SELECTION_CENTER),
+    BODY(DefaultExcelColor.rgb(255, 255, 255),
+            DefaultExcelBorders.newInstance(ExcelBorderStyle.THIN),
+            DefaultExcelAlign.CENTER_SELECTION_CENTER);
+
+    private final ExcelColor color;
+    private final DefaultExcelBorders borders;
+    private final ExcelAlign excelAlign;
+
+    DefaultExcelCellStyle(ExcelColor color, DefaultExcelBorders borders, ExcelAlign excelAlign) {
+        this.color = color;
+        this.borders = borders;
+        this.excelAlign = excelAlign;
+    }
+
     @Override
     public void apply(CellStyle cellStyle) {
-        // 기본으로 두기
+        color.applyForeground(cellStyle);
+        borders.apply(cellStyle);
+        excelAlign.apply(cellStyle);
     }
 }
