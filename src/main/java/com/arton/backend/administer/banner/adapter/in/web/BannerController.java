b4addb5e255c9fca5ada1e87691981321c432d99
thdrmsqhd <thdrmsqhd@gmail.com>
2023-02-24 00:51:28 +0000

    ART-60 배너 테스트 구조 추가

--- src/main/java/com/arton/backend/administer/banner/adapter/in/web/BannerController.java
+++ src/main/java/com/arton/backend/administer/banner/adapter/in/web/BannerController.java
@@ -1,5 +1,13 @@
 package com.arton.backend.administer.banner.adapter.in.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/banner")
+@RequiredArgsConstructor
 public class BannerController {
     
 }
