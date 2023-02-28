2a532fb61a9d4325d714b2007be4583d5d3ef01f
godcoder <dwrhksfl77@naver.com>
2023-02-28 13:11:52 +0900

    ART-73 그룹 메일 발송
    - 날짜 조건 확인 완료.
    - 수신 조건 확인 필요.

--- src/main/java/com/arton/backend/administer/mail/application/data/AdminMailSearchDto.java
+++ src/main/java/com/arton/backend/administer/mail/application/data/AdminMailSearchDto.java
@@ -3,6 +3,7 @@ package com.arton.backend.administer.mail.application.data;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -15,7 +16,9 @@ public class AdminMailSearchDto {
     @Schema(description = "수신동의")
     private String termsAgree;
     @Schema(description = "가입일 지정 시작일")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate gteDate;
     @Schema(description = "가입일 지정 마지막일")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate ltDate;
 }
