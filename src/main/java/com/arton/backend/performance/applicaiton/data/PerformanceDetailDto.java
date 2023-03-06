6cc09392dd6e7837a6e7d9bf508f7964f9378a7f
a60156077 <dwrhksfl77@naver.com>
2023-03-06 13:36:41 +0900

    fix 홈 화면 데이터
    - 메인 페이지 performance 추가 정보가 필요하다고 하여 dto 양식 수정.

--- src/main/java/com/arton/backend/performance/applicaiton/data/PerformanceDetailDto.java
+++ src/main/java/com/arton/backend/performance/applicaiton/data/PerformanceDetailDto.java
@@ -5,8 +5,12 @@ import com.arton.backend.price.application.data.PriceInfoDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -21,9 +25,15 @@ public class PerformanceDetailDto {
     private List<PriceInfoDto> prices = new ArrayList<>();
     private Integer purchaseLimit;
     private Integer limitAge;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    @Schema(description = "티켓팅 오픈일")
+    private String ticketOpenDate;
+    @Schema(description = "공연 종료일")
+    private String ticketEndDate;
 
     @Builder
-    public PerformanceDetailDto(Long id, List<String> images, String title, String place, String musicalDateTime, List<PriceInfoDto> prices, Integer purchaseLimit, Integer limitAge) {
+    public PerformanceDetailDto(Long id, List<String> images, String title, String place, String musicalDateTime, List<PriceInfoDto> prices, Integer purchaseLimit, Integer limitAge, LocalDateTime startDate, LocalDateTime endDate, String ticketOpenDate, String ticketEndDate) {
         this.id = id;
         this.images = images;
         this.title = title;
@@ -32,9 +42,17 @@ public class PerformanceDetailDto {
         this.prices = prices;
         this.purchaseLimit = purchaseLimit;
         this.limitAge = limitAge;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.ticketOpenDate = ticketOpenDate;
+        this.ticketEndDate = ticketEndDate;
     }
 
     public static PerformanceDetailDto toDto(Performance performance, List<String> images, List<PriceInfoDto> prices) {
+        String[] ticketStartDate = performance.getTicketOpenDate().format(DateTimeFormatter.ofPattern("MM.dd HH:mm")).split(" ");
+        String ticketStartTextDay = performance.getTicketOpenDate().getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.KOREAN);
+        String[] ticketEndDate = performance.getTicketEndDate().format(DateTimeFormatter.ofPattern("MM.dd HH:mm")).split(" ");
+        String ticketEndTextDay = performance.getTicketEndDate().getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.KOREAN);
         return PerformanceDetailDto.builder()
                 .id(performance.getPerformanceId())
                 .images(images)
@@ -44,6 +62,31 @@ public class PerformanceDetailDto {
                 .prices(prices)
                 .purchaseLimit(performance.getPurchaseLimit())
                 .limitAge(performance.getLimitAge())
+                .startDate(performance.getStartDate())
+                .endDate(performance.getEndDate())
+                .ticketOpenDate(ticketStartDate[0]+"("+ticketStartTextDay+")"+" "+ticketStartDate[1])
+                .ticketEndDate(ticketEndDate[0]+"("+ticketEndTextDay+")"+" "+ticketEndDate[1])
+                .build();
+    }
+
+    public static PerformanceDetailDto toDto(Performance performance) {
+        String[] ticketStartDate = performance.getTicketOpenDate().format(DateTimeFormatter.ofPattern("MM.dd HH:mm")).split(" ");
+        String ticketStartTextDay = performance.getTicketOpenDate().getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.KOREAN);
+        String[] ticketEndDate = performance.getTicketEndDate().format(DateTimeFormatter.ofPattern("MM.dd HH:mm")).split(" ");
+        String ticketEndTextDay = performance.getTicketEndDate().getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.KOREAN);
+        return PerformanceDetailDto.builder()
+                .id(performance.getPerformanceId())
+                .images(new ArrayList<>())
+                .title(performance.getTitle())
+                .place(performance.getPlace())
+                .musicalDateTime(performance.getMusicalDateTime())
+                .prices(new ArrayList<>())
+                .purchaseLimit(performance.getPurchaseLimit())
+                .limitAge(performance.getLimitAge())
+                .startDate(performance.getStartDate())
+                .endDate(performance.getEndDate())
+                .ticketOpenDate(ticketStartDate[0]+"("+ticketStartTextDay+")"+" "+ticketStartDate[1])
+                .ticketEndDate(ticketEndDate[0]+"("+ticketEndTextDay+")"+" "+ticketEndDate[1])
                 .build();
     }
 }
