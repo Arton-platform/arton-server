e9c1c2a48d06979389c8a1f12f162a93d222bc8c
a60156077 <dwrhksfl77@naver.com>
2023-03-16 09:54:35 +0900

    swagger 설명 추가
    - ArtistController: 회원 가입시 찜리스트에 보여줄 아티스트 기능 swagger 설명 추가.

--- src/main/java/com/arton/backend/artist/adapter/in/ArtistController.java
+++ src/main/java/com/arton/backend/artist/adapter/in/ArtistController.java
@@ -1,11 +1,20 @@
 package com.arton.backend.artist.adapter.in;
 
-import com.arton.backend.artist.application.port.in.ArtistUseCase;
 import com.arton.backend.artist.application.data.ArtistInterestDto;
 import com.arton.backend.artist.application.data.ListArtistInterestDto;
+import com.arton.backend.artist.application.port.in.ArtistUseCase;
+import com.arton.backend.auth.application.data.TokenDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -20,6 +29,10 @@ public class ArtistController {
      * 뮤지컬/콘서트 종류에 따라 아티스트 리스트를 보여준다.
      * Type: MUSICAL, CONCERT
      */
+    @Operation(summary = "회원가입시 공연 찜 리스트", description = "회원가입시 공연 찜 리스트를 가져옵니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "리스트 불러오기 성공",
+                    content = @Content(schema = @Schema(implementation = TokenDto.class)))})
     @GetMapping("/zzim")
     public ResponseEntity<ListArtistInterestDto> showAllArtistForZzim(@RequestParam(name = "performanceType", required = true) String performanceType) {
         List<ArtistInterestDto> artistZzimDtos = artistUseCase.showArtistListForZzim(performanceType);
