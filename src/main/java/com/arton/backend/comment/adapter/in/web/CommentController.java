b19d704a2ae8092c0a83c0b9c263571b2e5b964e
a60156077 <dwrhksfl77@naver.com>
2023-03-16 15:59:11 +0900

    swagger 설명 추가
    - CommentController: swagger api response 추가

--- src/main/java/com/arton/backend/comment/adapter/in/web/CommentController.java
+++ src/main/java/com/arton/backend/comment/adapter/in/web/CommentController.java
@@ -6,6 +6,13 @@ import com.arton.backend.comment.domain.Comment;
 import com.arton.backend.infra.shared.common.ResponseData;
 import com.arton.backend.performance.adapter.out.persistence.entity.PerformanceEntity;
 import com.arton.backend.review.domain.Review;
+import com.arton.backend.user.application.data.MyPageDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +29,10 @@ public class CommentController {
     private final CommentListUseCase commentListUseCase;
     private final CommentRegistUseCase commentRegistUseCase;
 
+    @Operation(summary = "공연 리뷰의 댓글보기", description = "해당 공연의 특정 리뷰에 대한 댓글을 반환합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "코멘트 반환 성공",
+                    content = @Content(array = @ArraySchema(schema = @Schema( implementation = Comment.class))))})
     @GetMapping("/list/{performanceId}/{reviewId}")
     public ResponseEntity<ResponseData<List<Comment>>> commentList(@PathVariable("performanceId") Long performanceId, @PathVariable("reviewId") Long reviewId){
         PerformanceEntity performance = PerformanceEntity.builder()
@@ -40,7 +51,10 @@ public class CommentController {
 
         return ResponseEntity.ok().body(response);
     }
-
+    @Operation(summary = "공연 리뷰 댓글 등록", description = "해당 공연의 특정 리뷰에 대한 댓글을 등록합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "코멘트 등록 성공",
+                    content = @Content(array = @ArraySchema(schema = @Schema( implementation = Comment.class))))})
     @PostMapping("/regist")
     public ResponseEntity<ResponseData<Comment>> regist(@RequestBody Comment comment){
         ResponseData<Comment> response = new ResponseData<>(
