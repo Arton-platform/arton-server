51d20a0cbe6b0361b39629992c56b5c59d69e38e
a60156077 <dwrhksfl77@naver.com>
2023-04-18 14:50:02 +0900

    Revert "Mysql lines 예약어라 category 테이블 생성중 에러 발생."
    This reverts commit f72d418a66739a0ee86d0cba1bcec10a145ddcd7.

--- src/main/java/com/arton/backend/administer/category/domain/CategoryEntity.java
+++ src/main/java/com/arton/backend/administer/category/domain/CategoryEntity.java
@@ -1,6 +1,9 @@
 package com.arton.backend.administer.category.domain;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,14 +15,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 @Getter
-@Table(name = "category")
 public class CategoryEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
     int level; // 카테고리 트리 구조를 위한 계층 컬럼
-    @Column(name = "_lines")
     int lines; // 한 페이지에 몇개의 카테고리를 표현할지 결정하는 컬럼
     int width; // 한 카테고리에 몇개의 공연을 표현할지 결정하는 컬럼
     String url; // 분류 클릭시 이동할 페이지
