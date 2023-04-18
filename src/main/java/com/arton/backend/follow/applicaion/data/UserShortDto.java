6207c0f944d18b9ef2e78dac2d82118a937cc4de
a60156077 <dwrhksfl77@naver.com>
2023-04-18 14:01:11 +0900

    json 타입 수정 follow -> isFollow로 표기되게.

--- src/main/java/com/arton/backend/follow/applicaion/data/UserShortDto.java
+++ src/main/java/com/arton/backend/follow/applicaion/data/UserShortDto.java
@@ -1,9 +1,9 @@
 package com.arton.backend.follow.applicaion.data;
 
 import com.arton.backend.user.domain.User;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
-@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
@@ -12,7 +12,28 @@ public class UserShortDto {
     private String nickname;
     private String selfDescription;
     // 팔로워 여부 추가
+    @JsonProperty(value = "isFollow")
     private boolean isFollow;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setFollow(boolean isFollow){
+        this.isFollow = isFollow;
+    }
 
     public static UserShortDto to(User user) {
         return UserShortDto.builder()
@@ -29,9 +50,5 @@ public class UserShortDto {
                 .selfDescription(user.getSelfDescription())
                 .isFollow(isFollow)
                 .build();
-    }
-
-    public void setFollow(boolean isFollow){
-        this.isFollow = isFollow;
     }
 }
