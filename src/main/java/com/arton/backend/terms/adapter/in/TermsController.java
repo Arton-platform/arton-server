95c506671f1be585466d7d4bf0bf22c587e6f3a1
a60156077 <dwrhksfl77@naver.com>
2023-04-25 17:59:46 +0900

    약관 페이지 DB -> url 방식으로 다시 변경.

--- src/main/java/com/arton/backend/terms/adapter/in/TermsController.java
+++ src/main/java/com/arton/backend/terms/adapter/in/TermsController.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,9 +36,10 @@ public class TermsController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "약관 정보 반환 성공",
                     content = @Content(array = @ArraySchema(schema = @Schema(implementation = TermsShowDto.class))))})
-    public ResponseData<List<TermsShowDto>> showTermList() {
+    public ResponseEntity<ResponseData<List<TermsShowDto>>> showTermList() {
         List<TermsShowDto> response = termsService.getTerms();
-        return new ResponseData<>("SUCCESS", HttpStatus.OK.value(), response);
+        ResponseData responseData = new ResponseData("SUCCESS", HttpStatus.OK.value(), response);
+        return ResponseEntity.ok(responseData);
     }
 
 //    @GetMapping
