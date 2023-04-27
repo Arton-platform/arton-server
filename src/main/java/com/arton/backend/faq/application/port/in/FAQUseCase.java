6426063a30b481188d272cc04279c6b08d314c40
a60156077 <dwrhksfl77@naver.com>
2023-04-27 09:51:27 +0900

    faq response 형식 변경.

--- src/main/java/com/arton/backend/faq/application/port/in/FAQUseCase.java
+++ src/main/java/com/arton/backend/faq/application/port/in/FAQUseCase.java
@@ -1,10 +1,25 @@
 package com.arton.backend.faq.application.port.in;
 
+import com.arton.backend.faq.application.data.FAQListResponseDTO;
+import com.arton.backend.faq.application.data.FAQResponseDTO;
 import com.arton.backend.faq.domain.FAQ;
 
 import java.util.List;
 
 public interface FAQUseCase {
     List<FAQ> faqList();
+    /**
+     * 동건님 요청사항 반영 response dto
+     * 20230427
+     * @return
+     */
+    List<FAQListResponseDTO> faqListV2();
     FAQ getFaqById(long id);
+    /**
+     * 동건님 요청사항 반영 response dto
+     * 20230427
+     * @param id
+     * @return
+     */
+    FAQResponseDTO getFaqByIdV2(long id);
 }
