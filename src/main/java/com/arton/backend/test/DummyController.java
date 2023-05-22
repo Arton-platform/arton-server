a8dd4feaf06b815fd029dd1b7fa539c2f789ddea
godcoder <dwrhksfl77@naver.com>
2023-05-23 00:02:21 +0900

    공지사항, faq 더미 생성 기능 완료...

--- src/main/java/com/arton/backend/test/DummyController.java
+++ src/main/java/com/arton/backend/test/DummyController.java
@@ -1,10 +1,24 @@
 package com.arton.backend.test;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/dummy")
 @RequiredArgsConstructor
 public class DummyController {
     private final DummyService dummyService;
+
+    @GetMapping("/announcement")
+    public ResponseEntity createDummyAnnouncements() {
+        dummyService.createDummyAnnouncements();
+        return ResponseEntity.ok("더미 공지사항 생성 완료...");
+    }
+    @GetMapping("/faq")
+    public ResponseEntity createDummyFAQs() {
+        dummyService.createDummyFAQ();
+        return ResponseEntity.ok("더미 faq 생성 완료...");
+    }
+
 }
