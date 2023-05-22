445968cba5bd30646e2a577585121a544731d9b9
godcoder <dwrhksfl77@naver.com>
2023-05-23 00:10:04 +0900

    오타 수정..

--- src/main/java/com/arton/backend/test/DummyController.java
+++ src/main/java/com/arton/backend/test/DummyController.java
@@ -3,9 +3,11 @@ package com.arton.backend.test;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/dummy")
+@RestController
+@RequestMapping("/dummy")
 @RequiredArgsConstructor
 public class DummyController {
     private final DummyService dummyService;
