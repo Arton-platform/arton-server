8ca5cdb404abe292ab04eb7847d4151b7b7f0b1b
a60156077 <dwrhksfl77@naver.com>
2023-06-27 10:35:43 +0900

    review create dto 수정.

--- src/main/java/com/arton/backend/review/application/data/ReviewCreateDto.java
+++ src/main/java/com/arton/backend/review/application/data/ReviewCreateDto.java
@@ -8,10 +8,14 @@ import lombok.NoArgsConstructor;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.PositiveOrZero;
 
+/**
+ * parentId를 명시하면 대댓글이고 아니라면 해당 댓글이 root가 됨.
+ */
 @Data
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ReviewCreateDto {
     private Long performanceId;
+    private Long parentId;
     @NotBlank
     private String content;
     @PositiveOrZero
