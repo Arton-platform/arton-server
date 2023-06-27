670841d41e73d6380ac876bee975bceb338337d9
a60156077 <dwrhksfl77@naver.com>
2023-06-27 10:15:22 +0900

    대댓글 기능을 위해 리뷰 엔터티 업데이트.

--- src/main/java/com/arton/backend/review/adapter/out/persistence/entity/ReviewEntity.java
+++ src/main/java/com/arton/backend/review/adapter/out/persistence/entity/ReviewEntity.java
@@ -2,6 +2,7 @@ package com.arton.backend.review.adapter.out.persistence.entity;
 
 import com.arton.backend.infra.shared.BaseEntity;
 import com.arton.backend.performance.adapter.out.persistence.entity.PerformanceEntity;
+import com.arton.backend.review.domain.Review;
 import com.arton.backend.user.adapter.out.persistence.entity.UserEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -10,6 +11,8 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -20,24 +23,33 @@ public class ReviewEntity extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "performance_id")
     private PerformanceEntity performance;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private UserEntity user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private ReviewEntity parent;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "parent", orphanRemoval = true)
+    private List<ReviewEntity> children = new ArrayList<>();
 
     private String content;
     private float starScore;
     private Long hit;
 
-    @Builder
-    public ReviewEntity(LocalDateTime createdDate, LocalDateTime updatedDate, long id, PerformanceEntity performance, UserEntity user, String content, float starScore, Long hit) {
+    public ReviewEntity(LocalDateTime createdDate, LocalDateTime updatedDate, Long id, PerformanceEntity performance, UserEntity user, ReviewEntity parent, List<ReviewEntity> children, String content, float starScore, Long hit) {
         super(createdDate, updatedDate);
         this.id = id;
         this.performance = performance;
         this.user = user;
+        this.parent = parent;
+        this.children = children;
         this.content = content;
         this.starScore = starScore;
         this.hit = hit;
