616ec82d05b1305e25532a5b128adcc1f58fa230
a60156077 <dwrhksfl77@naver.com>
2023-06-27 16:09:27 +0900

    리뷰 좋아요 엔터티, 매퍼 구현.

---
+++ src/main/java/com/arton/backend/reviewhit/adapter/out/persistence/mapper/ReviewHitMapper.java
@@ -0,0 +1,26 @@
+package com.arton.backend.reviewhit.adapter.out.persistence.mapper;
+
+import com.arton.backend.reviewhit.adapter.out.persistence.entitiy.ReviewHitEntity;
+import com.arton.backend.reviewhit.domain.ReviewHit;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReviewHitMapper {
+    public static ReviewHit toDomain(ReviewHitEntity reviewHit) {
+        return ReviewHit.builder()
+                .reviewId(reviewHit.getReviewId())
+                .userId(reviewHit.getUserId())
+                .createdDate(reviewHit.getCreatedDate())
+                .updatedDate(reviewHit.getUpdatedDate())
+                .build();
+    }
+
+    public static ReviewHitEntity toEntity(ReviewHit reviewHit) {
+        return ReviewHitEntity.builder()
+                .reviewId(reviewHit.getReviewId())
+                .userId(reviewHit.getUserId())
+                .createdDate(reviewHit.getCreatedDate())
+                .updatedDate(reviewHit.getUpdatedDate())
+                .build();
+    }
+}
