2845dc27f377cf71b971e9c346b0238c63021f33
a60156077 <dwrhksfl77@naver.com>
2023-06-30 13:56:02 +0900

    1. 리뷰 이미지 adapter 구현 완료..

--- src/main/java/com/arton/backend/image/adapter/out/persistence/repository/ReviewImageRepository.java
+++ src/main/java/com/arton/backend/image/adapter/out/persistence/repository/ReviewImageRepository.java
@@ -2,6 +2,15 @@ package com.arton.backend.image.adapter.out.persistence.repository;
 
 import com.arton.backend.image.adapter.out.persistence.entity.ReviewImageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ReviewImageRepository extends JpaRepository<ReviewImageEntity, Long> {
+    List<ReviewImageEntity> findAllByReviewId(Long reviewId);
+    @Modifying
+    @Query("DELETE FROM ReviewImageEntity r where r.review.id =: reviewId")
+    void deleteAllByReviewId(@Param(value = "reviewId") Long reviewId);
 }
